9d770073407ec742215694f826eaa4450d85991d
riptidewave93 <chrisrblake93@gmail.com>
2013-12-24 12:41:06 -0600

    Update Logo/Update Status page

--- modules/web_module_status.c
+++ modules/web_module_status.c
@@ -8,6 +8,7 @@ int web_module_status(void)
     FILE *ptr_file;
     char buf[1000];
 
+    // start page output
     printf("<font style=\"font-weight: bold; font-size: 1.5em; color:white\">Network Status</font><br/>");
     ptr_file = popen("busybox ifconfig | busybox grep -e \"inet:\" -e \"addr:\" | busybox grep -v \"inet6\" | busybox grep -v \"127.0.0.1\" | busybox head -n 1 | busybox awk '{print $2}' | busybox cut -c6-","r");
     while (fgets(buf,1000, ptr_file)!=NULL)
@@ -40,17 +41,16 @@ int web_module_status(void)
     }
     pclose(ptr_file);
 
-    ptr_file=popen("cat /sys/devices/platform/mv88de3100-hwmon.0/hwmon/hwmon0/tsen_temp","r");
-    while (fgets(buf,1000, ptr_file)!=NULL)
-    {
-        printf( "CPU Temp: <font style=\"color:white;\"> <b>%sC</b></font> <br />", buf );
-    }
-    pclose(ptr_file);
-
     ptr_file=popen("cat /factory/serial.txt | busybox tr '\n' ' '","r");
     while (fgets(buf,1000, ptr_file)!=NULL)
     {
         printf( "Serial #: <font style=\"color:white;\"> <b>%s</b></font> <br />", buf );
+    }
+    pclose(ptr_file);
+    ptr_file=popen("cat /sys/devices/platform/mv88de3100-hwmon.0/hwmon/hwmon0/tsen_temp","r");
+    while (fgets(buf,1000, ptr_file)!=NULL)
+    {
+        printf( "CPU Temp: <font style=\"color:white;\"> <b>%sC</b></font> <br />", buf );
     }
     pclose(ptr_file);
 }
